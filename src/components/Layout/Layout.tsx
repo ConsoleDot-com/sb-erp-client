0d9494bd30b7aa94d873c7bdf84f807a5507a051
Shuja Ur Rehman <shuja0094@gmail.com>
2023-07-19 12:21:06 +0500

    Update Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,24 +9,24 @@ type DefaultLayoutProps = {
 export const Layout = ({ children }: DefaultLayoutProps) => {
   return (
     <Box
-      sx={{
-        backgroundImage: `url(${bg})`,
-        backgroundRepeat: "no-repeat",
-        minHeight: "100vh",
-        backgroundSize: "cover",
-        position: "relative",
-        zIndex: 0,
-        "&::before": {
-          content: '""',
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          minHeight: "100%",
-          background: "rgba(255,255,255,0.4)",
-          zIndex: -1,
-        },
-      }}
+      // sx={{
+      //   backgroundImage: `url(${bg})`,
+      //   backgroundRepeat: "no-repeat",
+      //   minHeight: "100vh",
+      //   backgroundSize: "cover",
+      //   position: "relative",
+      //   zIndex: 0,
+      //   "&::before": {
+      //     content: '""',
+      //     position: "absolute",
+      //     top: 0,
+      //     left: 0,
+      //     width: "100%",
+      //     minHeight: "100%",
+      //     background: "rgba(255,255,255,0.4)",
+      //     zIndex: -1,
+      //   },
+      // }}
     >
       <NavBar />
       {children}
